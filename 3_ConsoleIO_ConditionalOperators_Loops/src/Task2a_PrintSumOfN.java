a4aa1c462f867c820f8a97e9ccc76cc236ec34a4
Martin <MartinDochev>
2016-10-02 22:43:51 +0300

    Task2g, Task2i, Task3d, Task3e not done yet. Rest of the Tasks work fine.

--- 3_ConsoleIO_ConditionalOperators_Loops/src/Task2a_PrintSumOfN.java
+++ 3_ConsoleIO_ConditionalOperators_Loops/src/Task2a_PrintSumOfN.java
@@ -10,7 +10,6 @@ public class Task2a_PrintSumOfN {
         int insertNumber; // Variable for the inserted numbers.
         int sumResult = 0; // Variable for the sum of all inserted numbers
         do { // loop for summing the numbers
-            System.out.print("Insert numbers you want to sum: ");
             insertNumber = scan.nextInt();
             sumResult += insertNumber;
             amount--;
