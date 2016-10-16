e46f483c9ec0ed9979a48bff20d242912a2c5c5d
marti <marti@DESKTOP-JAAGRH6>
2016-10-16 23:45:22 +0300

    Task1

---
+++ 8_OOPExtraFeatures/Task1_School/src/Class.java
@@ -0,0 +1,19 @@
+
+public class Class extends School{
+ 
+    String classID;
+    Teacher[] teach;
+    public Class(String name, String classID) {
+        super(name);
+        this.setClassID(classID);
+        this.teach = teach;
+    }
+
+    void setClassID(String ID) {
+      
+        this.classID = ID;
+    }
+ 
+    
+    
+}
---
+++ 8_OOPExtraFeatures/Task1_School/src/School.java
@@ -0,0 +1,13 @@
+
+public class School {
+    
+    String name;
+    
+    School (String name){
+        this.setSchoolName(name);
+    }
+    
+    void setSchoolName(String name){
+        this.name = name;
+    }
+}
