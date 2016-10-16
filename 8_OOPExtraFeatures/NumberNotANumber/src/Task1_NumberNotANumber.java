d2aea94e8e96e1bef1c3c02bd0ea88e0fdb0d828
marti <marti@DESKTOP-JAAGRH6>
2016-10-16 23:55:56 +0300

    Task1

---
+++ 8_OOPExtraFeatures/NumberNotANumber/src/Task1_NumberNotANumber.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+
+public class Task1_NumberNotANumber {
+    
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        String input = scan.nextLine();
+        
+        while (!input.equalsIgnoreCase("END")){
+            try {
+                Integer.parseInt(input);
+                System.out.println("Number");        
+                
+            }catch (NumberFormatException ex){
+                System.out.println("Not a number");
+            }
+            
+            input = scan.nextLine();
+            
+        }
+    }
+}
